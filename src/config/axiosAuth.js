a875e8490caf392c4fbba4963dc739d082e656db
samuelborraccini <samuelborraccini@hotmail.com>
2024-01-15 16:41:37 -0300

    cleanup

--- src/config/axiosAuth.js
+++ src/config/axiosAuth.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const token = localStorage.getItem("token") 
 
 const clienteAxiosAuth = axios.create({
-    baseURL:process.env.REACT_APP_URL,
+    baseURL:process.env.REACT_APP_AUTH_API_URL,
     headers: {
         'x-token': token
       }
