9e87c6709e8db1f6a9c65e8d28ff0ead51f1138f
samuelborraccini <samuelborraccini@hotmail.com>
2024-02-07 12:53:26 -0300

    user fix edit

--- src/pages/usuario/index.js
+++ src/pages/usuario/index.js
@@ -4,57 +4,57 @@ import { loadingAction } from "actions/helperActions";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import SoftBox from "components/SoftBox";
 import Card from "@mui/material/Card";
-import ListHeader from "components/ListHeader"
-import clienteAxios from 'config/axios';
+import ListHeader from "components/ListHeader";
+import clienteAxios from "config/axios";
 import { useNavigate } from "react-router-dom";
-import MUIDataTable from "mui-datatables"
-import { muiOptions,  columnsFunc2 } from "components/DataTable/options"
+import MUIDataTable from "mui-datatables";
+import { muiOptions, columnsFunc2 } from "components/DataTable/options";
 import clienteAxiosAuth from "config/axiosAuth";
+import { errorSwal } from "config/helpers";
 
+function Usuarios() {
+  const [rows, setRows] = useState([]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-function Usuarios (){
-    const [rows, setRows] = useState([])
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-
-    const getData = async()=>{
-        dispatch(loadingAction())
-        //const data = await clienteAxios.get('users/');
-        const data2 = await clienteAxiosAuth.get("/auth");
-        console.log(data2.data);
-        dispatch(loadingAction())
-        let respData = data2.data
-        let tempRows = respData.map(r=>{
-            return[r.first_name, r.email, r.profile.type, r.id]
-        })
-        setRows(tempRows)
-    }
-    useEffect(()=>{
-        getData()
-    },[])
-    const edit = (item)=> {
-        navigate(`/usuario/edit/${item}`);
-    }
-    const onDelete = (item) => {
-        clienteAxiosAuth.delete(`auth/${item}`)
-            .then(() => {
-            getData();
-        })
-    }
-    const columns = columnsFunc2(["Nombre", "Email", "Rol"],edit,3, onDelete);
-    return (
-        <DashboardLayout>
-            <SoftBox py={3}>
-            <SoftBox mb={3}>
-                <Card>
-                <ListHeader url="/usuarios/create" label="Listado Usuarios" buttonText="Agregar +"/>
-                <SoftBox>
-                    <MUIDataTable data={rows} columns={columns}options={muiOptions}/>
-                </SoftBox>
-                </Card>
+  const getData = async () => {
+    dispatch(loadingAction());
+    //const data = await clienteAxios.get('users/');
+    const data2 = await clienteAxiosAuth.get("/auth");
+    console.log(data2.data);
+    dispatch(loadingAction());
+    let respData = data2.data;
+    let tempRows = respData.map((r) => {
+      return [r.first_name, r.email, r.profile.type, r.id];
+    });
+    setRows(tempRows);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const edit = (item) => {
+    navigate(`/usuario/edit/${item}`);
+  };
+  const onDelete = (item) => {
+    /* clienteAxiosAuth.delete(`auth/${item}`).then(() => {
+      getData();
+    }); */
+    errorSwal("No se pueden eliminar usuarios, solo desactivar");
+  };
+  const columns = columnsFunc2(["Nombre", "Email", "Rol"], edit, 3, onDelete);
+  return (
+    <DashboardLayout>
+      <SoftBox py={3}>
+        <SoftBox mb={3}>
+          <Card>
+            <ListHeader url="/usuarios/create" label="Listado Usuarios" buttonText="Agregar +" />
+            <SoftBox>
+              <MUIDataTable data={rows} columns={columns} options={muiOptions} />
             </SoftBox>
-            </SoftBox>
-        </DashboardLayout>
-    );
+          </Card>
+        </SoftBox>
+      </SoftBox>
+    </DashboardLayout>
+  );
 }
-export default Usuarios
+export default Usuarios;
